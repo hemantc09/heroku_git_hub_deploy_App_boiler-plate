a38581c6dd136473350eae6141f7f62ac7721a77
“hemantc09” <hemantc09@gmail.com>
2018-12-06 09:01:12 -0800

    updated webpack config for production build

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,13 @@
 
 const path = require('path');
 // console.log(path.join(__dirname, 'public'));
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+
+    return {
     entry: './src/app.js',
     output: {
         path: path.join(__dirname, 'public'),
@@ -20,16 +25,31 @@ module.exports = {
         },{
             //make css and scss file extension
             test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
+            use: CSSExtract.extract({
+                use: [
+                    {
+                        loader:'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader:'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                   
+                ]
+            })
         }]
     },
-    devtool: 'cheap-module-eval-source-map',
+    plugins: [
+        CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
         contentBase: path.join(__dirname, 'public')
     }
-};
-
+    }
+}
