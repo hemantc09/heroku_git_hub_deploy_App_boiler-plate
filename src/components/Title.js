002110c7777e41ed7cad1dfa7aa21666a5bacfb4
“hemantc09” <hemantc09@gmail.com>
2018-12-13 17:55:57 -0800

    create boiler plate

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,7 +4,7 @@ export default class Title extends  React.Component  {
     
     render(){
         return(
-            <h1 className="title title-spectrum "> React Chat app demo</h1>
+            <h1 className="title title-spectrum "> heroku + git hub + deploy App + boiler plate</h1>
         );
     }
 }
